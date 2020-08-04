ef5d22b11fa104c564af950b5b716e7358a9ed16
park <msmn1729@gmail.com>
2020-08-04 20:41:50 +0900

    ADD BOJ

---
+++ BOJ/BOJ/2751.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <cstring>
+//#include <queue>
+//#include <string>
+//#include <map>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//    cout << setprecision(16);
+//    
+//    int n, i, ar[1000000]={};
+//    cin >> n;
+//    for(i=0; i<n; i++)
+//    {
+//        cin >> ar[i];
+//    }
+//    
+//    sort(ar, ar+i);
+//    for(i=0; i<n; i++)
+//    {
+//        cout << ar[i] << '\n';
+//    }
+//}
