6283f0cb19d8892f0c0ed4fbd90c07e46dd0d15e
park <msmn1729@gmail.com>
2020-08-11 17:48:56 +0900

    ADD BOJ

--- BOJ/BOJ/1874.cpp
+++ BOJ/BOJ/1874.cpp
@@ -12,7 +12,7 @@
 //using namespace std;
 //
 //stack<int> stk;
-//vector<int> v;
+//vector<char> v;
 //
 //int main(int argc, const char * argv[]) {
 //    ios_base::sync_with_stdio(false);
@@ -25,13 +25,20 @@
 //    int num[n];
 //    for(i=0; i<n; i++)
 //    {
-//        cin >> num[i];
+//        cin >> num[i]; //num배열에 만들수열 순서대로 저장
 //    }
 //    for(i=1; i<=n; i++)
 //    {
-//        stk.push(i);
-//        v.push_back('+');
-//
+//        stk.push(i); //스택에 숫자 순서대로 푸쉬
+//        v.push_back('+'); //푸쉬할 때 연산자 + 푸쉬백
+//        while(stk.empty() == false && num[index] == stk.top())
+//        {
+//            stk.pop();
+//            v.push_back('-');
+//            index++;
+//        }
 //    }
+//    if(stk.empty() == false) cout << "NO";
+//    else for(auto it:v) cout << it << endl;
 //}
 //
