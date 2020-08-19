3327456dfccb153024fb49991f3c01255fa63615
park <msmn1729@gmail.com>
2020-08-19 16:09:07 +0900

    ETC_01644_소수들의 연속합

---
+++ BOJ/ETC/ETC_01644_소수들의 연속합.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+//https://www.acmicpc.net/problem/1644 소수의 연속합 골드3
+//소수와 투 포인터
+//하지만 소수처리만 해줘도 통과됨
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n, i, j, flag, ans=0;
+    vector<int> v;
+    
+    cin >> n;
+
+    for(i=2; i<=n; i++)
+    {
+        flag = 1;
+        for(j=2; j*j<=i; j++)
+        {
+            if(i % j == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag) //소수인 경우
+        {
+            v.push_back(i);
+        }
+    }
+    
+    //내가 푼 방법
+//    for(i=(int)v.size()-1; i>=0; i--)
+//    {
+//        int tmp = n;
+//        if(n >= v[i]) //값보다 작은 소수일 때
+//        {
+//            for(j=0; i-j>=0;j++)
+//            {
+//                tmp -= v[i-j];
+//                if(tmp == 0)
+//                {
+//                    ans++;
+//                    break;
+//                }
+//                else if(tmp < 0 ) break;
+//            }
+//        }
+//    }
+
+    //투 포인터방식
+    int start=0, end=0, sum=0;
+
+    while(1)
+    {
+        //부분합이 n보다 큰 경우
+        if(sum >= n)
+        {
+            sum = sum - v[start++];
+        }
+        else if(end == v.size()) break;
+        else //부분합이 n보다 작은 경우
+        {
+            sum = sum + v[end++];
+        }
+        if(sum == n) ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
