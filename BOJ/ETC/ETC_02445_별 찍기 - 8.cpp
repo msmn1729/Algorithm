2d91a90f787eeb1253ed6f4b2dda5b182b4082cc
park <msmn1729@gmail.com>
2020-08-23 22:05:54 +0900

    ETC_02445_별 찍기 - 8

---
+++ BOJ/ETC/ETC_02445_별 찍기 - 8.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+
+//https://www.acmicpc.net/problem/2445 별 찍기 - 8
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int i, j, n;
+    cin >> n;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            cout << '*';
+        }
+        for(j=0; j <= 2*n - 2*i - 3; j++)
+        {
+            cout << ' ';
+        }
+        for(j=0; j<=i; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    
+    for(i=0; i<n-1; i++)
+    {
+        for(j=0; j< n - i - 1; j++)
+        {
+            cout << '*';
+        }
+        for(j=0; j < i*2 + 2; j++)
+        {
+            cout << ' ';
+        }
+        for(j=0; j< n - i - 1; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
