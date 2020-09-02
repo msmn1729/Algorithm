e8c986bda5159388d4b6c9fbcf25eadf509fc71e
park <msmn1729@gmail.com>
2020-09-03 01:26:37 +0900

    ETC_12813_이진수 연산

---
+++ BOJ/ETC/ETC_12813_이진수 연산.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    bitset<100000> a, b;
+    cin >> a >> b;
+    cout << (a&b) << '\n';
+    cout << (a|b) << '\n';
+    cout << (a^b) << '\n';
+    cout << ~a << '\n';
+    cout << ~b << '\n';
+}
