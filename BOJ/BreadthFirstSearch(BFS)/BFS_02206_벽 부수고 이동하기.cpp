a63b9c1eee2a6dac281102c84d3fe42f8b8fd143
park <msmn1729@gmail.com>
2020-10-07 18:05:39 +0900

    BFS_02206_벽 부수고 이동하기

--- BOJ/BreadthFirstSearch(BFS)/BFS_02206_벽 부수고 이동하기.cpp
+++ BOJ/BreadthFirstSearch(BFS)/BFS_02206_벽 부수고 이동하기.cpp
@@ -15,63 +15,67 @@
 using namespace std;
 
 //https://www.acmicpc.net/problem/2206 벽 부수고 이동하기
-//페어, 튜플, 구조체
-//dist를 정의할 때 고민해보자
 
-string board[1001];
-int dist[1001][1001][2];
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-int r, c;
-
-struct location
+struct loc
 {
-    int x, y, w;
+    int x, y, z;
 };
 
-int bfs()
+int dir[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+int board[1001][1001] = {};
+int dist[1001][1001][2] = {}; //마지막 인덱스는 부순 벽의 개수
+
+int main()
 {
-    queue<location> q;
-    q.push({0, 0, 0}); //시작좌표
-    dist[0][0][0] = 1; //방문처리
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cout.tie(0);
     
+    int r, c, i, j;
+    scanf("%d %d", &r, &c);
+    
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            scanf("%1d", &board[i][j]);
+        }
+    }
+    queue<loc> q;
+    q.push({0, 0, 0});
+    dist[0][0][0] = 1;
     while(q.size())
     {
-        auto cur = q.front(); q.pop();
-        int x = cur.x, y = cur.y, w = cur.w;
-        if(x+1 == r && y+1 == c) return dist[x][y][w]; //도착점에 도달하면 정답 반환
+        int x = q.front().x;
+        int y = q.front().y;
+        int z = q.front().z; q.pop();
         
-        for(int dir=0; dir<4; dir++)
+        if(x == r-1 && y == c-1)
         {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            printf("%d\n", dist[x][y][z]);
+            return 0;
+        }
+        for(i=0; i<4; i++)
+        {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            
             if(nx<0 || ny<0 || nx>=r || ny>=c) continue;
-            if(dist[nx][ny][w]) continue; //방문한 곳은 패스
-            if(board[nx][ny] == '0') //빈 곳인 경우는 이전의 벽 부숨 여부가 상관없음
+            if(dist[nx][ny][z]) continue; //방문했거나 패스
+            
+            if(board[nx][ny] == 0) //벽이 아닐 때는 벽 부숨 여부가 상관없음
             {
-                dist[nx][ny][w] = dist[x][y][w] + 1;
-                q.push({nx, ny, w});
+                dist[nx][ny][z] = dist[x][y][z] + 1;
+                q.push({nx, ny, z});
             }
-            if(board[nx][ny] == '1' && w == 0) //처음 벽을 부시는 경우
+            else if(board[nx][ny] && !z) //벽을 처음 부술 때
             {
-                dist[nx][ny][1] = dist[x][y][w] + 1;
-                q.push({nx, ny, 1});
+                dist[nx][ny][z+1] = dist[x][y][z] + 1;
+                q.push({nx, ny, z+1});
             }
         }
     }
-    return -1;
-}
-
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    cin >> r >> c;
-    int i;
-    for(i=0; i<r; i++) cin >> board[i];
-    cout << bfs();
-    
+    puts("-1");
     return 0;
 }
+
