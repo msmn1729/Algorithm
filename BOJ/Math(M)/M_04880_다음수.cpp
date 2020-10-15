15dd4b5d5d87443555de515f3d7a57529297392b
park <msmn1729@gmail.com>
2020-10-15 19:38:39 +0900

    M_04880_다음수

--- BOJ/Math(M)/M_04880_다음수.cpp
+++ BOJ/Math(M)/M_04880_다음수.cpp
@@ -1,9 +1,37 @@
-//
-//  M_04880_다음수.cpp
-//  BOJ
-//
-//  Created by Park GeonYeol on 2020/10/14.
-//  Copyright © 2020 Park GeonYeol. All rights reserved.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <set>
 
-#include <stdio.h>
+using namespace std;
+
+//https://www.acmicpc.net/problem/4880 다음수
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    while(1)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(!a && !b && !c) break;
+        if(a+c == 2*b) cout << "AP " << 2*c-b << '\n';
+        else cout << "GP " << c*c/b << '\n';
+    }
+    return 0;
+    
+    return 0;
+}
+
