a86d81d39d6e20a65204de4d17c1105344843ca5
park <msmn1729@gmail.com>
2020-11-02 14:24:18 +0900

    ETC_10991_별 찍기 - 16

---
+++ BOJ/ETC/ETC_10991_별 찍기 - 16.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <set>
+#define ll long long
+
+using namespace std;
+
+//https://www.acmicpc.net/problem/10991 별 찍기 - 16
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, i, j;
+    cin >> n;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n-i-1; j++) cout << ' ';
+        for(j=0; j<=i; j++) cout << "* ";
+        cout << '\n';
+    }
+    
+    return 0;
+}
