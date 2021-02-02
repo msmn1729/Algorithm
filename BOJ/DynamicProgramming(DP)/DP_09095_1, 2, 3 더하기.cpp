c2ca924da93d994df1a9ae34a45ad781d30033de
park <msmn1729@gmail.com>
2021-02-03 01:47:29 +0900

    DP_09095_1, 2, 3 더하기 : 범위 수정

--- BOJ/DynamicProgramming(DP)/DP_09095_1, 2, 3 더하기.cpp
+++ BOJ/DynamicProgramming(DP)/DP_09095_1, 2, 3 더하기.cpp
@@ -29,7 +29,7 @@ int main() {
     cin >> t;
     
     int dp[11] = {0, 1, 2, 4};
-    for(int i=4; i<=11; i++) dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+    for(int i=4; i<11; i++) dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
     while(t--) {
         cin >> n;
         cout << dp[n] << '\n';
