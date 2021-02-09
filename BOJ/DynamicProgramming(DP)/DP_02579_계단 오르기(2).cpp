bf972c372695bf6a8c965fe2e04cf103adb75ebf
park <msmn1729@gmail.com>
2021-02-09 17:38:32 +0900

    DP_02579_계단 오르기(2)

--- BOJ/DynamicProgramming(DP)/DP_02579_계단 오르기(2).cpp
+++ BOJ/DynamicProgramming(DP)/DP_02579_계단 오르기(2).cpp
@@ -1,9 +1,47 @@
-//
-//  DP_02579_계단 오르기(2).cpp
-//  BOJ
-//
-//  Created by Park GeonYeol on 2021/02/03.
-//  Copyright © 2021 Park GeonYeol. All rights reserved.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <map>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <bitset>
+#define ll long long
+#define INF 1e9
 
-#include <stdio.h>
+using namespace std;
+
+//https://www.acmicpc.net/problem/2579 계단 오르기
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int ar[301] = {};
+    int dp[301][3] = {};
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) cin >> ar[i];
+    if(n == 1) {
+        cout << ar[1];
+        return 0;
+    }
+    dp[1][1] = ar[1];
+    dp[2][1] = ar[2];
+    dp[2][2] = ar[1] + ar[2];
+    for(int i=3; i<=n; i++) {
+        dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + ar[i];
+        dp[i][2] = dp[i-1][1] + ar[i];
+    }
+    cout << max(dp[n][1], dp[n][2]);
+    
+    return 0;
+}
