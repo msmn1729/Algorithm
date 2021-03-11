9972543feb719d01522f279119c804325e77ae98
park <msmn1729@gmail.com>
2021-03-11 15:22:36 +0900

    BFS_07576_토마토

--- BOJ/BreadthFirstSearch(BFS)/BFS_07576_토마토.cpp
+++ BOJ/BreadthFirstSearch(BFS)/BFS_07576_토마토.cpp
@@ -1,74 +1,76 @@
-#include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
+#include <iostream>
+#include <climits>
 #include <cstring>
-#include <queue>
-#include <string>
-#include <map>
 #include <iomanip>
-#include <list>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
 #include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+#define ll long long
+#define INF 1e9
 
 using namespace std;
 
 //https://www.acmicpc.net/problem/7576 토마토
 
-int board[1002][1002];
-int dist[1002][1002];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
 
-int n, m;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
-    cin >> m >> n;
-    int i, j, ans=0;
-    queue<pair<int, int> > Q;
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            cin >> board[i][j];
-            if(board[i][j] == 1) //토마토면
-            {
-                Q.push({i, j}); //토마토의 좌표들을 큐에 저장
-            }
-            else if(board[i][j] == 0) //안익은 토마토면
-            {
-                dist[i][j] = -1; //가야할 곳
-            }
+    
+    int ar[1001][1001] = {};
+    int dist[1001][1001] = {};
+    int r, c;
+    queue<pair<int, int> > q;
+    cin >> c >> r;
+    
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            cin >> ar[i][j];
+            if(ar[i][j] == 1) q.push({i, j});
+            else if(ar[i][j] == 0) dist[i][j] = -1; // 방문할 좌표들 -1로 초기화
         }
     }
-
-    while(!Q.empty())
-    {
-        auto cur = Q.front();
-        Q.pop();
-        for(int div=0; div<4; div++)
-        {
-            int nx = cur.first + dx[div];
-            int ny = cur.second + dy[div];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(dist[nx][ny] >= 0) continue; //방문했었거나(dist>0) 토마토가 없는 곳(dist==0)이면 패스
-            dist[nx][ny] = dist[cur.first][cur.second] + 1;
-            ans = max(ans, dist[nx][ny]);
-            Q.push({nx, ny});
+    
+    int ans = 0;
+    while(q.size()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for(int dir=0; dir<4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if(nx<0 || ny<0 || nx>=r || ny>=c) continue;
+            if(dist[nx][ny] >= 0) continue; // 0인 경우는 빈칸인 곳이라 갈 수가 없음
+            
+            dist[nx][ny] = dist[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            if(dist[i][j] == -1) //안익은 토마토가 남아있으면
-            {
+    
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            if(dist[i][j] == -1) {
                 cout << -1;
                 return 0;
             }
+            ans = max(ans, dist[i][j]);
         }
     }
     cout << ans;
+    
+    return 0;
 }
