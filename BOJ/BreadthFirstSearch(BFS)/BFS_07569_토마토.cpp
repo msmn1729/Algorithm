c880f24866c908b3b6186eebf2a4dd57de1a443c
park <msmn1729@gmail.com>
2021-03-11 15:47:03 +0900

    BFS_07569_토마토

--- BOJ/BreadthFirstSearch(BFS)/BFS_07569_토마토.cpp
+++ BOJ/BreadthFirstSearch(BFS)/BFS_07569_토마토.cpp
@@ -1,96 +1,86 @@
-#include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
+#include <iostream>
+#include <climits>
 #include <cstring>
-#include <queue>
-#include <string>
-#include <map>
 #include <iomanip>
-#include <list>
-#include <stack>
-#include <cstring>
+#include <bitset>
+#include <string>
+#include <vector>
 #include <cmath>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
 #include <set>
+
+#define ll long long
+#define INF 1e9
 
 using namespace std;
 
 //https://www.acmicpc.net/problem/7569 토마토
-//bfs익히기에 좋은 문제!
 
-int board[101][101][101];
-int dist[101][101][101];
-int n, m, h;
-int dx[6] = {0, 0, -1, 0, 1, 0};
-int dy[6] = {0, 0, 0, 1, 0, -1};
-int dz[6] = {-1, 1, 0, 0, 0, 0};
+int dx[6] = {-1, 0, 1, 0, 0, 0};
+int dy[6] = {0, -1, 0, 1, 0, 0};
+int dz[6] = {0, 0, 0, 0, -1, 1};
 
-int main()
-{
+struct loc{
+    int z, x, y;
+};
+
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
-    int i, j, k;
-    queue<tuple<int, int, int> > q;
-
-    cin >> m >> n >> h;
     
-    for(i=0; i<h; i++)
-    {
-        for(j=0; j<n; j++)
-        {
-            for(k=0; k<m; k++)
-            {
-                cin >> board[i][j][k];
-                if(board[i][j][k] == 1) //토마토 좌표 저장
-                {
-                    q.push({i, j, k});
-                }
-                else if(board[i][j][k] == 0) //안 익은 토마토
-                {
-                    dist[i][j][k] = -1; //가야하는 곳
-                }
+    int ar[101][101][101] = {};
+    int dist[101][101][101] = {};
+    int r, c, h;
+    queue<loc> q;
+    cin >> c >> r >> h;
+    
+    for(int k=0; k<h; k++) {
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                cin >> ar[k][i][j];
+                if(ar[k][i][j] == 1) q.push({k, i, j}); //익은 토마토 좌표 삽입
+                else if(ar[k][i][j] == 0) dist[k][i][j] = -1; //방문할 좌표들 -1로 초기화
             }
         }
     }
     
-    int mx = 0;
-    while(!q.empty())
-    {
-        auto cur = q.front(); q.pop();
-        for(int i=0; i<6; i++)
-        {
-            int nz, ny, nx;
-            tie(nz, ny, nx) = cur;
-            nz += dz[i];
-            ny += dy[i];
-            nx += dx[i];
-            if(nz<0 || nz>=h || ny<0 || ny>=n || nx<0 || nx>=m) continue;
-            if(dist[nz][ny][nx] >= 0) continue; //토마토가 익어있거나 없는 경우
+    int ans = 0;
+    while(q.size()) {
+        int z = q.front().z;
+        int x = q.front().x;
+        int y = q.front().y;
+        q.pop();
+        
+        for(int dir=0; dir<6; dir++) {
+            int nz = z + dz[dir];
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if(nx<0 || ny<0 || nz<0 || nx>=r || ny>=c || nz>=h) continue;
+            if(dist[nz][nx][ny] >= 0) continue;
             
-            dist[nz][ny][nx] = dist[nz-dz[i]][ny-dy[i]][nx-dx[i]] + 1;
-            q.push({nz, ny, nx});
-            mx = max(mx, dist[nz][ny][nx]);
+            dist[nz][nx][ny] = dist[z][x][y] + 1;
+            q.push({nz, nx, ny});
         }
     }
-    for(i=0; i<h; i++)
-    {
-        for(j=0; j<n; j++)
-        {
-            for(k=0; k<m; k++)
-            {
-                if(dist[i][j][k] == -1)
-                {
+    for(int k=0; k<h; k++) {
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                if(dist[k][i][j] == -1) {
                     cout << -1;
                     return 0;
                 }
-//                cout << dist[i][j][k] << ' ';
+                ans = max(ans, dist[k][i][j]);
             }
-//            cout << '\n';
         }
-//        cout << '\n';
     }
-    cout << mx;
+    cout << ans;
     
     return 0;
 }
