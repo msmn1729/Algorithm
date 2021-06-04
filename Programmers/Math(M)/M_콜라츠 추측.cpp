3e4a3de8fa8f3609a27458d015506b0e96292696
geonyeolpark <msmn1729@gmail.com>
2021-06-04 20:00:54 +0900

    M_콜라츠 추측.cpp

---
+++ Programmers/Math(M)/M_콜라츠 추측.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#define ll long long
+
+using namespace std;
+
+int solution(int n) {
+    ll num = n;
+    int answer = 0;
+    
+    while(num > 1) {
+        if(num % 2 == 0) num /= 2;
+        else num = (num * 3) + 1;
+        answer++;
+        if(answer >= 500) return -1;
+    }
+    
+    return answer;
+}
