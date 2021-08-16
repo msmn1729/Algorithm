4d1177ae409029c205a53df41b9438deb2c98521
Geonyeol Park <msmn1729@gmail.com>
2021-08-16 16:14:34 +0900

    Create DP_15990_1, 2, 3 더하기 5.cpp

---
+++ BOJ/DynamicProgramming(DP)/DP_15990_1, 2, 3 더하기 5.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+// https://www.acmicpc.net/problem/15990 1, 2, 3 더하기 5
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    ll dp[101010][4] = {};
+    int mod = 1e9 + 9;
+    
+    dp[1][1] = 1;
+    dp[2][2] = 1;
+    dp[3][1] = 1;
+    dp[3][2] = 1;
+    dp[3][3] = 1;
+    
+    for(int i=4; i<=100000; i++) {
+        dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % mod;
+        dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % mod;
+        dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % mod;
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << (dp[n][1] + dp[n][2] + dp[n][3]) % mod << '\n';
+    }
+    
+    return 0;
+}
