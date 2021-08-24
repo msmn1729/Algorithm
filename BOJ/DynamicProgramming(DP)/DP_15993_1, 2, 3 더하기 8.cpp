1d2285750986ec4b62177b717f30f3d1d9c371a2
Geonyeol Park <msmn1729@gmail.com>
2021-08-24 21:02:04 +0900

    Create DP_15993_1, 2, 3 더하기 8.cpp

---
+++ BOJ/DynamicProgramming(DP)/DP_15993_1, 2, 3 더하기 8.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+// https://www.acmicpc.net/problem/15992 1, 2, 3 더하기 8
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    ll dp1[101010] = {}, dp2[101010] = {};
+    int mod = 1e9 + 9;
+    
+    dp1[1] = 1;
+    dp1[2] = 1;
+    dp2[2] = 1;
+    dp1[3] = 2;
+    dp2[3] = 2;
+    
+    for(int i=4; i<=100000; i++) {
+        dp1[i] = (dp2[i-1] + dp2[i-2] + dp2[i-3]) % mod;
+        dp2[i] = (dp1[i-1] + dp1[i-2] + dp1[i-3]) % mod;
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << dp1[n] << ' ' << dp2[n] << '\n';
+    }
+    
+    return 0;
+}
