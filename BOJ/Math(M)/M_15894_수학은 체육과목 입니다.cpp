2e7548dfa61af1e4d4b1968c3b786ebc20f044b1
Geonyeol Park <msmn1729@gmail.com>
2022-10-08 20:59:16 +0900

    Create M_15894_수학은 체육과목 입니다.cpp

---
+++ BOJ/Math(M)/M_15894_수학은 체육과목 입니다.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    cout << (ll)n * 4;
+        
+    return 0;
+}
