2371c9b6ab2c100ee37fd24c324565a8ab3efca9
Geonyeol Park <msmn1729@gmail.com>
2022-10-30 23:52:24 +0900

    Create ETC_15688_수 정렬하기 5.cpp

---
+++ BOJ/ETC/ETC_15688_수 정렬하기 5.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#define ll long long
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    
+    for(int i=0; i<n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    for(int i=0; i<n; i++) cout << v[i] << '\n';
+
+    return 0;
+}
