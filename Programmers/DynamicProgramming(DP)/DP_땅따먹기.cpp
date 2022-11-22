969098e26b8a8ff3c86139b91a4c1d71a0729cd0
Geonyeol Park <msmn1729@gmail.com>
2022-11-22 18:54:59 +0900

    Create DP_땅따먹기.cpp

---
+++ Programmers/DynamicProgramming(DP)/DP_땅따먹기.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<int> > land) {
+    int answer = 0;
+    int dp[100001][4] = {};
+    
+    for(int i=0; i<4; i++) {
+        dp[0][i] = land[0][i];
+    }   
+    
+    for(int i=1; i<land.size(); i++) {
+        for(int j=0; j<4; j++) {
+            for(int k=0; k<4; k++) {
+                if(j == k) continue;
+                dp[i][j] = max(dp[i][j], dp[i-1][k] + land[i][j]);
+            }
+        }
+    }
+    
+    for(int i=0; i<4; i++) {
+        answer = max(answer, dp[land.size()-1][i]);
+    }
+
+    return answer;
+}
