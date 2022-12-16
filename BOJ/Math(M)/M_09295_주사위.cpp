cde97b735665181b863fc4d6b65ee5f50535ed5d
Geonyeol Park <msmn1729@gmail.com>
2022-12-16 23:41:22 +0900

    Create M_09295_주사위.cpp

---
+++ BOJ/Math(M)/M_09295_주사위.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << '\n';
+    }
+    
+    return 0;
+}
+
