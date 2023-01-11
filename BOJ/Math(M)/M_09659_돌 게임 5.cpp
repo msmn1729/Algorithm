1c5b0555b1148a7237fa866337e5233b5ba91469
Geonyeol Park <msmn1729@gmail.com>
2023-01-11 23:23:12 +0900

    Create M_09659_돌 게임 5.cpp

---
+++ BOJ/Math(M)/M_09659_돌 게임 5.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 1e9
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin >> n;
+    
+    if(n % 2) cout << "SK";
+    else cout << "CY";
+    
+    return 0;
+}
