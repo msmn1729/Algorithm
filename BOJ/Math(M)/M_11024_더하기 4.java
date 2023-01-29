ffd6bcc025783ea072de4c7f0ec772cf2841cc2c
Geonyeol Park <msmn1729@gmail.com>
2023-01-30 00:28:51 +0900

    Create M_11024_더하기 4.java

---
+++ BOJ/Math(M)/M_11024_더하기 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            System.out.println(sum);
+        }
+    }
+}
