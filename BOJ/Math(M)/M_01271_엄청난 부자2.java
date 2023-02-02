cec8834246a2b0a5c1e18cbc6f8f77d123d74655
Geonyeol Park <msmn1729@gmail.com>
2023-02-02 22:30:56 +0900

    Create M_01271_엄청난 부자2.java

---
+++ BOJ/Math(M)/M_01271_엄청난 부자2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
